173e7e17b77441c50c661b85ffe79eaa92db233e
EzequieldeSR <ezequieldesr@gmail.com>
2026-02-22 11:04:42 -0300

    feature: adiciona metodo adiciona, aula03

---
+++ src/com/ezequiel/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package com.ezequiel.estruturadados.vetor.teste;
+
+import com.ezequiel.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+	}
+
+}
---
+++ src/com/ezequiel/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.ezequiel.estruturadados.vetor.teste;
+
+import com.ezequiel.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		
+	}
+
+}
